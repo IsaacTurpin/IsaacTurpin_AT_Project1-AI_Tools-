a15b0817d3f758732281d90fbb5ec6e401567c8b
IsaacTurpin <Isaac2.Turpin@live.uwe.ac.uk>
2023-11-01 12:02:08 +0000

    mouse can now move camera
    notes: took 8 questions - cannot do controller, still trying

--- Project 1 - AI Tools/Assets/CameraFollow.cs
+++ Project 1 - AI Tools/Assets/CameraFollow.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;  // Reference to the player's transform
-    public Vector3 offset;    // Offset of the camera from the player
+    public Transform player; // Reference to the player's transform
+    public float rotationSpeed = 3f; // Speed of camera rotation using mouse
+    public float controllerRotationSpeed = 150f; // Speed of camera rotation using controller right stick
+    public float smoothness = 0.1f; // Smoothing factor for camera movement
 
     void LateUpdate()
     {
-        if (target != null)
-        {
-            // Set the position of the camera to the player's position + offset
-            transform.position = target.position + offset;
+        // Rotate the player based on mouse movement
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        player.Rotate(Vector3.up * mouseX);
 
-            // Ensure the camera looks at the player's position without rotating in the x-axis
-            Vector3 lookAtPosition = new Vector3(target.position.x, transform.position.y, target.position.z);
-            transform.LookAt(lookAtPosition);
-        }
+        // Rotate the player based on controller right stick movement
+        float rightStickX = Input.GetAxis("RightStickX") * controllerRotationSpeed * Time.deltaTime;
+        player.Rotate(Vector3.up * rightStickX);
+
+        // Calculate the desired camera position based on player's position
+        Vector3 targetPosition = player.position - player.forward * 3f + Vector3.up * 2f; // Example values, adjust as needed
+
+        // Smoothly interpolate the camera's position towards the target position
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothness);
+
+        // Ensure the camera looks at the player's position without rotating in the x-axis
+        Vector3 lookAtPosition = new Vector3(player.position.x, transform.position.y, player.position.z);
+        transform.LookAt(lookAtPosition);
     }
 }
+
+
+
+
+
+
+
+
+
 
 
 
