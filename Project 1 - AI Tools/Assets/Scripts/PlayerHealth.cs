ecfcb0f03b28ce006503b96f3f191ceb4a76d335
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2023-11-16 16:18:42 +0000

    added player health bar
    Notes: 2 questions, did great

--- Project 1 - AI Tools/Assets/Scripts/PlayerHealth.cs
+++ Project 1 - AI Tools/Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
 
+    public Slider healthSlider; // Reference to the UI slider
+
     void Start()
     {
         currentHealth = maxHealth;
+
+        // Make sure to call this method to initialize the UI slider
+        InitializeHealthBar();
+    }
+
+    void InitializeHealthBar()
+    {
+        // Update the UI slider value based on the player's current health
+        float fillAmount = (float)currentHealth / maxHealth;
+        healthSlider.value = fillAmount;
     }
 
     public void TakeDamage(int damage)
@@ -17,6 +30,13 @@ public class PlayerHealth : MonoBehaviour
         currentHealth -= damage;
 
         // Add logic for handling damage effects, such as screen shake, sound effects, or UI updates
+
+        // Make sure health doesn't go below zero
+        currentHealth = Mathf.Max(currentHealth, 0);
+
+        // Update the UI slider value based on the player's current health
+        float fillAmount = (float)currentHealth / maxHealth;
+        healthSlider.value = fillAmount;
 
         if (currentHealth <= 0)
         {
@@ -31,4 +51,5 @@ public class PlayerHealth : MonoBehaviour
         gameObject.SetActive(false);
     }
 }
+
 
