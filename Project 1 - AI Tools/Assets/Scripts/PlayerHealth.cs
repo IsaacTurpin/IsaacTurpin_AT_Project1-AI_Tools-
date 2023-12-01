ac757cfae8d643b8dd36f19c064bbdfd5075b391
IsaacTurpin <Isaac2.Turpin@live.uwe.ac.uk>
2023-12-01 12:11:11 +0000

    Player death added
    death anim plays then reloads the level. Notes: 3 questions, great, similar to previous

--- Project 1 - AI Tools/Assets/Scripts/PlayerHealth.cs
+++ Project 1 - AI Tools/Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
@@ -8,11 +9,22 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 100;
     private int currentHealth;
 
+    private Animator animator;
+    private bool hasDied = false;
+
     public Slider healthSlider; // Reference to the UI slider
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        // Get the Animator component
+        animator = GetComponent<Animator>();
+
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on the player.");
+        }
 
         // Make sure to call this method to initialize the UI slider
         InitializeHealthBar();
@@ -46,9 +58,29 @@ public class PlayerHealth : MonoBehaviour
 
     void Die()
     {
+        // Check if the player has already died to avoid multiple calls
+        if (hasDied)
+        {
+            return;
+        }
+
         // Add logic for player death behavior (e.g., play death animation, game over screen, etc.)
-        // For now, let's simply deactivate the player object
-        gameObject.SetActive(false);
+
+        // Play the player death animation
+        if (animator != null)
+        {
+            animator.SetTrigger("Die");
+        }
+
+        // Set a flag to avoid multiple calls
+        hasDied = true;
+    }
+
+    // Called by an Animation Event when the player death animation is complete
+    void PlayerDieAnimationFinished()
+    {
+        // Restart the level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
 
